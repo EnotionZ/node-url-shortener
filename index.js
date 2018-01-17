acfe7b5f170ac9d3c16f137505e359fcd16739c2
Dominick Pham <dominick.pham@gmail.com>
2018-01-16 16:28:22 -0800

    options for nus

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var config = require('./config/opts');
-var nus = require('./lib/nus')(config);
 config['__dirname'] = __dirname;
 
 // express app
 module.exports = function(app, opts) {
     opts = Object.assign(config, opts);
+    var nus = require('./lib/nus')(opts);
 
     // Load express configuration
     require('./config/env')(app, opts);
